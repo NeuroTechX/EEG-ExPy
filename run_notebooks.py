2ac1349ba622953f2b9546c0cf8bb00ad280b9c0
Surya Pandiaraju <surya.pandiaraju@gmail.com>
2020-10-08 11:02:50 -0400

    Update run_notebooks.py
    added auditory_erp_arrayin experiment to run from command line (still need to test)

--- run_notebooks.py
+++ run_notebooks.py
@@ -5,6 +5,7 @@ from eegnb.devices.eeg import EEG
 from eegnb.experiments.visual_n170 import n170
 from eegnb.experiments.visual_p300 import p300
 from eegnb.experiments.visual_ssvep import ssvep
+from eegnb.experiments.auditory_oddball import auditory_erp_arrayin
 
 
 def intro_prompt():
@@ -102,6 +103,24 @@ def main():
         p300.present(duration=record_duration, eeg=eeg_device, save_fn=save_fn)
     elif experiment == 'visual-SSVEP':
         ssvep.present(duration=record_duration, eeg=eeg_device, save_fn=save_fn)
+    elif experiment == 'auditory-MMN':
+        conditions_file = 'MUSE_conditions.mat'
+        F = h5py.File(conditions_file, 'r')#['museEEG']
+        highPE = np.squeeze(F['museEEG']['design']['highPE'][:]).astype(int)
+        lowPE = np.squeeze(F['museEEG']['design']['lowPE'][:]).astype(int)
+        inputs = np.squeeze(F['museEEG']['design']['inputs'][:]).astype(int)
+        oddball = np.squeeze(F['museEEG']['design']['oddball'][:]).astype(int)
+        oddball-=1 
+        stim_types = oddball
+        itis = np.ones_like(oddball)*0.5
+        newAdditionalMarkers = [];
+        for i in range(0, len(highPE)):
+            newAdditionalMarker = str(stim_types[i]) + str(highPE[i]) + str(lowPE[i])
+            newAdditionalMarkers.append(newAdditionalMarker)
+        additional_labels = {'labels' : newAdditionalMarkers}
+
+        auditory_erp_arrayin.present(record_duration=record_duration,stim_types=stim_types,itis=itis,
+                                        additional_labels = {'labels' : newAdditionalMarkers}, eeg=eeg_device, save_fn=save_fn)
 
 
 if __name__=="__main__":
