8172f83a55026c966a35eca88a21bfacca7ab6f3
Surya Pandiaraju <surya.pandiaraju@gmail.com>
2020-10-16 14:18:27 -0400

    Update __init__.py

--- eegnb/__init__.py
+++ eegnb/__init__.py
@@ -22,3 +22,22 @@ def generate_save_fn(board_name, experiment, subject_id, session_nb, data_dir=DA
     save_fp = path.join(recording_dir, ("recording_%s" % strftime("%Y-%m-%d-%H.%M.%S", gmtime()) + ".csv"))
 
     return save_fp
+
+def generate_save_path(board_name, experiment, subject_id, session_nb, data_dir=DATA_DIR):
+    '''Generates a file name with the proper trial number for the current subject/experiment combo'''
+
+    # convert subject ID to 4-digit number
+    subject_str = 'subject%04.f' % subject_id
+    session_str = 'session%03.f' % session_nb
+
+    # folder structure is /DATA_DIR/experiment/site/subject/session/*.csv
+    recording_dir = path.join(data_dir, experiment, 'local', board_name, subject_str, session_str)
+
+    # check if directory exists, if not, make the directory
+    if not path.exists(recording_dir):
+        makedirs(recording_dir)
+
+    # generate filename based on recording date-and-timestamp and then append to recording_dir
+    save_path = path.join(recording_dir)
+
+    return save_path
