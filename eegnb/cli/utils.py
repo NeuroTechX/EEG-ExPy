8b1a9844ea7a09ca3dc0b6cc302555af19298495
Surya Pandiaraju <surya.pandiaraju@hotmail.com>
2020-10-19 11:34:11 -0400

    added cli

---
+++ eegnb/cli/utils.py
@@ -0,0 +1,15 @@
+
+from eegnb.experiments.visual_n170 import n170
+from eegnb.experiments.visual_p300 import p300
+from eegnb.experiments.visual_ssvep import ssvep
+
+def run_experiment(experiment, record_duration, eeg_device, save_fn):
+
+    if experiment == 'visual-N170':
+        n170.present(duration=record_duration, eeg=eeg_device, save_fn=save_fn)
+    elif experiment == 'visual-P300':
+        p300.present(duration=record_duration, eeg=eeg_device, save_fn=save_fn)
+    elif experiment == 'visual-SSVEP':
+        ssvep.present(duration=record_duration, eeg=eeg_device, save_fn=save_fn)
+
+
