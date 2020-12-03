7e4bc35d6649f273f68f5c37395281c9859fcfa9
Richard Waltman <retiutut@icloud.com>
2020-12-03 11:20:20 -0600

    Code Review - Remove unused code in utils.py related to listing serial ports

--- eegnb/devices/utils.py
+++ eegnb/devices/utils.py
@@ -85,31 +85,4 @@ def get_openbci_usb():
         return str(port_list[int(port_number)]).split(' - ')[0]
 
 def serial_ports():
-    """ Lists serial port names
-
-        :raises EnvironmentError:
-            On unsupported or unknown platforms
-        :returns:
-            A list of the serial ports available on the system
-    """
-    if sys.platform.startswith('win'):
-        ports = ['COM%s' % (i + 1) for i in range(256)]
-    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
-        # this excludes your current terminal "/dev/tty"
-        ports = glob.glob('/dev/tty[A-Za-z]*')
-    elif sys.platform.startswith('darwin'):
-        # Look for .cu instead of only for Mac so that it works with BrainFlow.
-        ports = glob.glob('/dev/cu.*')
-    else:
-        raise EnvironmentError('Unsupported platform')
-
-    #result = []
-    #for port in ports:
-        #try:
-            #s = serial.Serial(port)
-            #s.close()
-            #result.append(port)
-        #except (OSError, serial.SerialException):
-            #pass
-    #return result
     return serial.tools.list_ports.comports()
