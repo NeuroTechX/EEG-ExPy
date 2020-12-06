9b3371fa8b9c83df406280e49589b6162c7e7230
Erik Bjäreholt <erik@bjareho.lt>
2020-12-06 10:56:02 +0100

    style: formatted with `black eegnb/`

--- eegnb/cli/utils.py
+++ eegnb/cli/utils.py
@@ -1,4 +1,3 @@
-
 from eegnb.experiments.visual_n170 import n170
 from eegnb.experiments.visual_p300 import p300
 from eegnb.experiments.visual_ssvep import ssvep
@@ -12,12 +11,15 @@ import numpy as np, pandas as pd
 
 
 import eegnb
+
 eegnb_dir = os.path.dirname(eegnb.__file__)
-mcond_file = os.path.join(eegnb_dir, 'experiments', 'auditory_oddball', 'MUSE_conditions.mat')
+mcond_file = os.path.join(
+    eegnb_dir, "experiments", "auditory_oddball", "MUSE_conditions.mat"
+)
 
 
 def makeoddball(inputs, rep):
-    #based on inputs, creating oddball paradigms markers depending on "switch"
+    # based on inputs, creating oddball paradigms markers depending on "switch"
     value = inputs[0]
     count = 0
     markerArray = []
@@ -31,62 +33,75 @@ def makeoddball(inputs, rep):
         else:
             if count == rep + 1:
                 markerArray.append(2)
-            
+
             else:
                 markerArray.append(4)
             value = inputs[i]
             count = 1
     return markerArray
 
+
 def maketonesnums(num):
     newArray = []
     for i in range(num):
-        newArray.append(90000+i)
-    return newArray   
-      
+        newArray.append(90000 + i)
+    return newArray
+
 
 def run_experiment(experiment, record_duration, eeg_device, save_fn):
-    if experiment == 'visual-N170':
+    if experiment == "visual-N170":
         n170.present(duration=record_duration, eeg=eeg_device, save_fn=save_fn)
-    elif experiment == 'visual-P300':
+    elif experiment == "visual-P300":
         p300.present(duration=record_duration, eeg=eeg_device, save_fn=save_fn)
-    elif experiment == 'visual-SSVEP':
+    elif experiment == "visual-SSVEP":
         ssvep.present(duration=record_duration, eeg=eeg_device, save_fn=save_fn)
-    elif experiment == 'auditory-SSAEP':
+    elif experiment == "auditory-SSAEP":
         ssaep.present(duration=record_duration, eeg=eeg_device, save_fn=save_fn)
-    elif experiment == 'auditory-oddball':
-        #conditions_file = os.path.join(eegnb_file, 'experiments', 'auditory-oddball', "MUSE_conditions.mat")
-        F = h5py.File(mcond_file, 'r')#['museEEG']
-        highPE = np.squeeze(F['museEEG']['design']['highPE'][:]).astype(int)
-        lowPE = np.squeeze(F['museEEG']['design']['lowPE'][:]).astype(int)
-        inputs = np.squeeze(F['museEEG']['design']['inputs'][:]).astype(int)
+    elif experiment == "auditory-oddball":
+        # conditions_file = os.path.join(eegnb_file, 'experiments', 'auditory-oddball', "MUSE_conditions.mat")
+        F = h5py.File(mcond_file, "r")  # ['museEEG']
+        highPE = np.squeeze(F["museEEG"]["design"]["highPE"][:]).astype(int)
+        lowPE = np.squeeze(F["museEEG"]["design"]["lowPE"][:]).astype(int)
+        inputs = np.squeeze(F["museEEG"]["design"]["inputs"][:]).astype(int)
 
-        #based on inputs, creating oddball paradigms markers depending on "switch"
+        # based on inputs, creating oddball paradigms markers depending on "switch"
         tonenums = maketonesnums(1800)
         oddball3 = makeoddball(inputs, 3)
         oddball4 = makeoddball(inputs, 4)
         oddball5 = makeoddball(inputs, 5)
         oddball6 = makeoddball(inputs, 6)
 
-        #modifying 0s in PE definitions of tones that represent markers to 3s to avoid loss of trials instead of ignoring them
+        # modifying 0s in PE definitions of tones that represent markers to 3s to avoid loss of trials instead of ignoring them
         for i in range(len(highPE)):
             if highPE[i] == 0:
                 highPE[i] = 3
             if lowPE[i] == 0:
-                lowPE[i] = 3  
-           
-        #1 is standard/bottom, 2 is deviant/high, 3 is "baseline trial"
+                lowPE[i] = 3
+
+        # 1 is standard/bottom, 2 is deviant/high, 3 is "baseline trial"
 
         stim_types = inputs
-        itis = np.ones_like(inputs)*0.5 
+        itis = np.ones_like(inputs) * 0.5
 
-        newAdditionalMarkers = [];
+        newAdditionalMarkers = []
 
         for i in range(0, len(highPE)):
-            newAdditionalMarker = str(oddball3[i]) + str(oddball4[i]) + str(oddball5[i]) + str(oddball6[i]) + str(highPE[i]) + str(lowPE[i])
+            newAdditionalMarker = (
+                str(oddball3[i])
+                + str(oddball4[i])
+                + str(oddball5[i])
+                + str(oddball6[i])
+                + str(highPE[i])
+                + str(lowPE[i])
+            )
             newAdditionalMarkers.append(newAdditionalMarker)
 
-        additional_labels = {'labels' : newAdditionalMarkers}
-        aMMN.present(record_duration=record_duration,stim_types=stim_types,itis=itis, additional_labels = {'labels' : newAdditionalMarkers}, eeg=eeg_device, save_fn=save_fn)
-        
-
+        additional_labels = {"labels": newAdditionalMarkers}
+        aMMN.present(
+            record_duration=record_duration,
+            stim_types=stim_types,
+            itis=itis,
+            additional_labels={"labels": newAdditionalMarkers},
+            eeg=eeg_device,
+            save_fn=save_fn,
+        )
--- eegnb/experiments/visual_p300/p300_stripes.py
+++ eegnb/experiments/visual_p300/p300_stripes.py
@@ -6,10 +6,10 @@ from optparse import OptionParser
 from pylsl import StreamInfo, StreamOutlet
 
 
-def present(duration,subject,run):
+def present(duration, subject, run):
 
     # create
-    info = StreamInfo('Markers', 'Markers', 1, 0, 'int32', 'myuidw43536')
+    info = StreamInfo("Markers", "Markers", 1, 0, "int32", "myuidw43536")
 
     # next make an outlet
     outlet = StreamOutlet(info)
@@ -19,7 +19,7 @@ def present(duration,subject,run):
     start = time()
 
     n_trials = 2010
-    iti = .4
+    iti = 0.4
     soa = 0.3
     jitter = 0.2
     record_duration = np.float32(duration)
@@ -27,15 +27,14 @@ def present(duration,subject,run):
     # Setup log
     position = np.random.binomial(1, 0.15, n_trials)
 
-    trials = DataFrame(dict(position=position,
-                            timestamp=np.zeros(n_trials)))
+    trials = DataFrame(dict(position=position, timestamp=np.zeros(n_trials)))
 
     # graphics
-    mywin = visual.Window([1920, 1080], monitor="testMonitor", units="deg",
-                          fullscr=True)
-    grating = visual.GratingStim(win=mywin, mask='gauss', size=40, sf=2)
-    fixation = visual.GratingStim(win=mywin, size=0.2, pos=[0, 0], sf=0,
-                                  rgb=[1, 0, 0])
+    mywin = visual.Window(
+        [1920, 1080], monitor="testMonitor", units="deg", fullscr=True
+    )
+    grating = visual.GratingStim(win=mywin, mask="gauss", size=40, sf=2)
+    fixation = visual.GratingStim(win=mywin, size=0.2, pos=[0, 0], sf=0, rgb=[1, 0, 0])
 
     for ii, trial in trials.iterrows():
         # inter trial interval
@@ -43,7 +42,7 @@ def present(duration,subject,run):
 
         # onset
         grating.phase += np.random.rand()
-        pos = trials['position'].iloc[ii]
+        pos = trials["position"].iloc[ii]
         grating.ori = 90 * pos
         grating.draw()
         fixation.draw()
@@ -65,15 +64,30 @@ def present(duration,subject,run):
 def main():
     parser = OptionParser()
 
-    parser.add_option("-d", "--duration",
-                      dest="duration", type='int', default=120,
-                      help="duration of the recording in seconds.")
-    parser.add_option("-s", "--subject",
-                  dest="subject", type='int', default=1,
-                  help="subject number: must be an integer")
-    parser.add_option("-r", "--run",
-                  dest="run", type='int', default=1,
-                  help="run (session) number: must be an integer")
+    parser.add_option(
+        "-d",
+        "--duration",
+        dest="duration",
+        type="int",
+        default=120,
+        help="duration of the recording in seconds.",
+    )
+    parser.add_option(
+        "-s",
+        "--subject",
+        dest="subject",
+        type="int",
+        default=1,
+        help="subject number: must be an integer",
+    )
+    parser.add_option(
+        "-r",
+        "--run",
+        dest="run",
+        type="int",
+        default=1,
+        help="run (session) number: must be an integer",
+    )
 
     (options, args) = parser.parse_args()
     present(options.duration)
@@ -81,5 +95,5 @@ def main():
     present(options.n)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
