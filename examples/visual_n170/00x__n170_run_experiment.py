1c0668fe5b6432715d521972d3bd050fefc385db
Erik Bjäreholt <erik@bjareho.lt>
2021-02-21 09:03:58 +0100

    fix: fixed passing of incorrect types (caught by mypy)

--- examples/visual_n170/00x__n170_run_experiment.py
+++ examples/visual_n170/00x__n170_run_experiment.py
@@ -18,10 +18,11 @@ from eegnb.devices.eeg import EEG
 from eegnb.experiments.visual_n170 import n170
 
 # Define some variables
-board_name = 'muse'
-experiment = 'visual_n170'
-subject = 'test'
-record_duration=120
+board_name = "muse"
+experiment = "visual_n170"
+subject_id = 0
+session_nb = 0
+record_duration = 120
 
 ###################################################################################################
 # Initiate EEG device
@@ -31,7 +32,7 @@ record_duration=120
 eeg_device = EEG(device=board_name)
 
 # Create save file name
-save_fn = generate_save_fn(board_name, experiment, subject)
+save_fn = generate_save_fn(board_name, experiment, subject_id, session_nb)
 print(save_fn)
 
 ###################################################################################################  
