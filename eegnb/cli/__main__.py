7a749395fb53b01a9846eee0162da50dc88ded65
Erik Bjäreholt <erik@bjareho.lt>
2021-03-26 12:53:17 +0100

    feat: rewrite CLI to use click (#89)

--- eegnb/cli/__main__.py
+++ eegnb/cli/__main__.py
@@ -1,77 +1,88 @@
-import argparse
-import sys
-from .cli import CLI
+import click
 
 
+@click.group(name="eegnb")
 def main():
-    parser = argparse.ArgumentParser(
-        description="eeg-notebooks command line interface",
-        usage="""eegnb <command> [<args>]
-
-    Available commands:
-    ===================
-
-    runexp      Run experiment
-    ------
-                
-                Options:
-
-                -ed --eegdevice    EEG device
-                -ex --expt         Experiment to run
-                -rd --recdur       Recording duration
-                -of --outfname     Output filename
-                -ip --inprom       Use input prompt 
+    """eeg-notebooks command line interface"""
+    pass
 
 
-                Examples:
+@main.command()
+@click.option(
+    "-ex",
+    "--experiment",
+    help="Experiment to run",
+    prompt="Experiment to run",
+)
+@click.option("-ed", "--eegdevice", help="EEG device to use")
+@click.option("-ma", "--macaddr", help="MAC address of device to use (if applicable)")
+@click.option("-rd", "--recdur", help="Recording duration", type=float)
+@click.option("-of", "--outfname", help="Output filename")
+@click.option(
+    "-ip", "--prompt", help="Use interactive prompt to ask for parameters", is_flag=True
+)
+def runexp(
+    experiment: str,
+    eegdevice: str = None,
+    macaddr: str = None,
+    recdur: float = None,
+    outfname: str = None,
+    prompt: bool = False,
+):
+    """
+    Run experiment.
 
-                Run experiment explicitly defining all necessary parameters 
-                (eeg device, experiment, duration, output file)
-                This is the quickest way to run eeg-notebooks experiments, 
-                but requires knowledge of formatting for available options
+    Examples:
 
-                $ eegnb runexp -ed museS -ex visual-N170 -rd 10 -of test.csv
+    Run experiment explicitly defining all necessary parameters
+    (eeg device, experiment, duration, output file)
+    This is the quickest way to run eeg-notebooks experiments,
+    but requires knowledge of formatting for available options
 
-
-                Launch the interactive command line experiment setup+run tool
-                This takes you through every experiment parameter in order 
-                and figures out + runs the complete function calls for you
-
-                $ eegnb runexp -ip
+    $ eegnb runexp -ed museS -ex visual-N170 -rd 10 -of test.csv
 
 
+    Launch the interactive command line experiment setup+run tool
+    This takes you through every experiment parameter in order
+    and figures out + runs the complete function calls for you
+
+    $ eegnb runexp -ip
+    """
+    if prompt:
+        print("run command line prompt script")
+        from .introprompt import main as run_introprompt
+
+        run_introprompt()
+    else:
+        from .utils import run_experiment
+        from eegnb.devices.eeg import EEG
+
+        if eegdevice == "ganglion":
+            # if the ganglion is chosen a MAC address should also be proviced
+            eeg = EEG(device=eegdevice, mac_addr=macaddr)
+        else:
+            eeg = EEG(device=eegdevice)
+
+        run_experiment(experiment, eeg, recdur, outfname)
 
 
-    view        View live eeg stream
-    ----
+@main.command()
+@click.option("-ed", "--eegdevice", help="EEG device to use")
+@click.option("-vr", "--version", help="Viewer version (for muselsl)")
+def view():
+    """
+    View live EEG stream.
 
-                Options:
+    Examples: TODO
+    """
+    print("add viewer functionality here")
 
-                -ed --device       EEG device
-                -vt --version      Viewer version (for muselsl)
+    # args = parser.parse_args(sys.argv[2:])
+    # from . import view
+    # view(args.window, args.scale, args.refresh,
+    #     args.figure, args.version, args.backend)
 
-                
-                Examples:
-
-
-
-
-
-    """,
-    )
-
-    parser.add_argument("command", help="Command to run.")
-
-    # parse_args defaults to [1:] for args, but you need to
-    # exclude the rest of the args too, or validation will fail
-    args = parser.parse_args(sys.argv[1:2])
-
-    if not hasattr(CLI, args.command):
-        print("Incorrect usage. See help below.")
-        parser.print_help()
-        exit(1)
-
-    cli = CLI(args.command)
+    raise NotImplementedError
 
 
 if __name__ == "__main__":
