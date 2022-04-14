0e029b4bcd110406027c6a5ed83747130d7f2070
John Griffiths <j.davidgriffiths@gmail.com>
2022-04-14 11:31:46 -0400

    almost working simple function equivalents of nb scripts

--- eegnb/analysis/pipelines.py
+++ eegnb/analysis/pipelines.py
@@ -17,7 +17,7 @@ warnings.filterwarnings('ignore')
 from mne import Epochs,find_events
 
 # EEG-Notebooks functions
-from eegnb.analysis.utils import load_data,plot_conditions
+from eegnb.analysis.utils import load_csv_as_raw,plot_conditions
 from eegnb.datasets import fetch_dataset
 
 
@@ -25,28 +25,48 @@ from eegnb.datasets import fetch_dataset
 
 
 """
+
+# Usage:
+
+
+from pipelines import compute_erp,make_erp_plot
+import glob
+from collections import OrderedDict
+
+fnames = glob.glob('C:/Users/eeg_lab/.eegnb/data/visual-N170/eegnb_examples/muse2016/subject0001/session001/*.csv')
+
+'
 epoch_kwargs = dict(tmin=-0.1, tmax=0.6, baseline=None,
                     reject={'eeg': 5e-5}, preload=True, 
                     verbose=False, picks=[0,1,2,3],
                     event_id = OrderedDict(House=1,Face=2) )
-"""
 
-""""
+raw,erp = compute_erp(fnames,epoch_kwargs)
+
+
+
 plot_kwargs = dict(conditions = epoch_kwargs['event_id'],
                    ci=97.5, n_boot=1000, title='',
                    diff_waveform=None, #(1, 2))
                    channel_order=[1,0,2,3]))
+
+make_erp_plot(epochs,plot_kwargs)
+
+
+
 """
 
 
-
 # Manually adjust the ylims
-def compute_erp(fnames,epoch_kwargs,plot_kwargs=None,do_plot=False,verbose=True):
+def compute_erp(fnames,epoch_kwargs,plot_kwargs=None,do_plot=False,verbose=True,sfreq=256,
+                ch_ind=[1,2,3,4]):
+
+
 
 
     # Load data
     # ----------------------------
-    raw = load_csv_as_raw(fnames)
+    raw = load_csv_as_raw(fnames,sfreq=sfreq,ch_ind=ch_ind)
 
 
     # Filtering and cleaning
@@ -66,24 +86,22 @@ def compute_erp(fnames,epoch_kwargs,plot_kwargs=None,do_plot=False,verbose=True)
     if verbose:
         print('sample drop %: ', (1 - len(epochs.events)/len(events)) * 100)
 
+    return raw,epochs
 
 
-
-    # Epoch average
-    # ----------------------------
-
-    if do_plot:
-
-        fig, ax = plot_conditions(epochs, **plot_kwargs)
-
-        # Manually adjust the ylims
-        #for i in [0,2]: ax[i].set_ylim([-0.5,0.5])
-        #for i in [1,3]: ax[i].set_ylim([-1.5,2.5])
+def make_erp_plot(epochs,plot_kwargs):
 
 
-        # Make Plots
-        # -----------------------------
-        #raw.plot_psd(fmin=1, fmax=30);
+    fig, ax = plot_conditions(epochs, **plot_kwargs)
+
+    # Manually adjust the ylims
+    #for i in [0,2]: ax[i].set_ylim([-0.5,0.5])
+    #for i in [1,3]: ax[i].set_ylim([-1.5,2.5])
+
+
+    # Make Plots
+    # -----------------------------
+    #raw.plot_psd(fmin=1, fmax=30);
 
 
 
