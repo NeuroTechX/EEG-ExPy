f05f1c4919c5f635242c0c8cb574e259ed84a0e8
Parvfect <parvagrw02@gmail.com>
2022-06-05 00:25:17 +0100

    Modifications

--- eegnb/experiments/Experiment.py
+++ eegnb/experiments/Experiment.py
@@ -18,32 +18,46 @@ called according to the stage of the cycle
 
 class Experiment:
 
-    def __init_():
+    def __init_(self):
+        
+        # Should have overwriting property for different experiments
+        self.instruction_text = """\nWelcome to the {} experiment!\nStay still, focus on the centre of the screen, and try not to blink. \nThis block will run for %s seconds.\n
+        Press spacebar to continue. \n""".format(exp_name) 
+  
+    
+    def setup(self):
 
-
-    def present(duration=120, eeg: EEG=None, save_fn=None, n_trials=2010, exp_name=""):
-        """ Do the present operation for a bunch of experiments """
-      
-        # Setup Trial list -> Common in most
-        parameter = np.random.binomial(1, 0.5, n_trials)
-        trials = DataFrame(dict(parameter=parameter, timestamp=np.zeros(n_trials)))
+        # Setup Trial list -> Common in most (csv in Unicorn)
+        self.parameter = np.random.binomial(1, 0.5, n_trials)
+        self.trials = DataFrame(dict(parameter=parameter, timestamp=np.zeros(n_trials)))
 
         # Setup Graphics 
         mywin = visual.Window([1600, 900], monitor="testMonitor", units="deg", fullscr=True) 
         """ Does specific thing within Experiment Type to load stimulus data """
-
+        
+        # Needs to be overwritten by specific experiment
+        self.stim = self.load_stimulus()
+        
         # Show Instruction Screen
-        show_instructions(duration=duration)
+        self.show_instructions(duration=duration)
 
-        # Start EEG Stream, wait for signal to settle, and then pull timestamp for start point
-        if eeg:
-        if save_fn is None:  # If no save_fn passed, generate a new unnamed save file
+        # Establish save function
+        if self.save_fn is None:  # If no save_fn passed, generate a new unnamed save file
             random_id = random.randint(1000,10000)
-            save_fn = generate_save_fn(eeg.device_name, "visual_n170", random_id, random_id, "unnamed")
+            self.save_fn = generate_save_fn(eeg.device_name, experiement_id, random_id, random_id, "unnamed")
             print(
                 f"No path for a save file was passed to the experiment. Saving data to {save_fn}"
             )
-        eeg.start(save_fn, duration=record_duration + 5)
+                
+
+    
+    def present(self, duration=120, eeg: EEG=None, save_fn=None, n_trials=2010, exp_name=""):
+        """ Do the present operation for a bunch of experiments """
+      
+    
+        # Start EEG Stream, wait for signal to settle, and then pull timestamp for start point
+        if eeg:
+            eeg.start(save_fn, duration=record_duration + 5)
 
         start = time()
         
@@ -75,9 +89,10 @@ class Experiment:
         if eeg:
             eeg.stop()
 
+    
     def show_instructions(instruction_text):
     
-        instruction_text = instruction_text % duration
+        self.instruction_text = self.instruction_text % duration
 
         # graphics
         mywin = visual.Window([1600, 900], monitor="testMonitor", units="deg", fullscr=True)
