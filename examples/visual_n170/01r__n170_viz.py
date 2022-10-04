4e0507daf3a55a22638a17a8e9d65b8edf2f9bca
Giovanni <chiarion.giovanni@gmail.com>
2022-10-04 14:38:08 +0200

    reverting original format to 01r_n179_viz.py

--- examples/visual_n170/01r__n170_viz.py
+++ examples/visual_n170/01r__n170_viz.py
@@ -26,14 +26,13 @@ The final figure plotted at the end shows the N170 response ERP waveform.
 import os
 from collections import OrderedDict
 import warnings
-
-warnings.filterwarnings("ignore")
+warnings.filterwarnings('ignore')
 
 # MNE functions
-from mne import Epochs, find_events
+from mne import Epochs,find_events
 
 # EEG-Notebooks functions
-from eegnb.analysis.utils import load_data, plot_conditions
+from eegnb.analysis.utils import load_data,plot_conditions
 from eegnb.datasets import fetch_dataset
 
 # sphinx_gallery_thumbnail_number = 3
@@ -50,25 +49,18 @@ from eegnb.datasets import fetch_dataset
 
 ###################################################################################################
 
-eegnb_data_path = os.path.join(os.path.expanduser("~/"), ".eegnb", "data")
-n170_data_path = os.path.join(eegnb_data_path, "visual-N170", "eegnb_examples")
+eegnb_data_path = os.path.join(os.path.expanduser('~/'),'.eegnb', 'data')    
+n170_data_path = os.path.join(eegnb_data_path, 'visual-N170', 'eegnb_examples')
 
-# If dataset hasn't been downloaded yet, download it
+# If dataset hasn't been downloaded yet, download it 
 if not os.path.isdir(n170_data_path):
-    fetch_dataset(
-        data_dir=eegnb_data_path, experiment="visual-N170", site="eegnb_examples"
-    )
+    fetch_dataset(data_dir=eegnb_data_path, experiment='visual-N170', site='eegnb_examples');
 
 subject = 1
 session = 1
-raw = load_data(
-    subject,
-    session,
-    experiment="visual-N170",
-    site="eegnb_examples",
-    device_name="muse2016_bfn",
-    data_dir=eegnb_data_path,
-)
+raw = load_data(subject,session,
+                experiment='visual-N170', site='eegnb_examples', device_name='muse2016_bfn',
+                data_dir = eegnb_data_path)
 
 ###################################################################################################
 # Visualize the power spectrum
@@ -80,8 +72,8 @@ raw.plot_psd()
 # Filtering
 # ----------------------------
 
-raw.filter(1, 30, method="iir")
-raw.plot_psd(fmin=1, fmax=30)
+raw.filter(1,30, method='iir')
+raw.plot_psd(fmin=1, fmax=30);
 
 ###################################################################################################
 # Epoching
@@ -89,22 +81,14 @@ raw.plot_psd(fmin=1, fmax=30)
 
 # Create an array containing the timestamps and type of each stimulus (i.e. face or house)
 events = find_events(raw)
-event_id = {"House": 1, "Face": 2}
+event_id = {'House': 1, 'Face': 2}
 
 # Create an MNE Epochs object representing all the epochs around stimulus presentation
-epochs = Epochs(
-    raw,
-    events=events,
-    event_id=event_id,
-    tmin=-0.1,
-    tmax=0.6,
-    baseline=None,
-    reject={"eeg": 5e-5},
-    preload=True,
-    verbose=False,
-    picks=[0, 1, 2, 3],
-)
-print("sample drop %: ", (1 - len(epochs.events) / len(events)) * 100)
+epochs = Epochs(raw, events=events, event_id=event_id, 
+                tmin=-0.1, tmax=0.6, baseline=None,
+                reject={'eeg': 5e-5}, preload=True, 
+                verbose=False, picks=[0,1,2,3])
+print('sample drop %: ', (1 - len(epochs.events)/len(events)) * 100)
 epochs
 
 ###################################################################################################
@@ -112,21 +96,16 @@ epochs
 # ----------------------------
 
 conditions = OrderedDict()
-conditions["House"] = [1]
-conditions["Face"] = [2]
+conditions['House'] = [1]
+conditions['Face'] = [2]
 
-fig, ax = plot_conditions(
-    epochs,
-    conditions=conditions,
-    ci=97.5,
-    n_boot=1000,
-    title="",
-    diff_waveform=None,  # (1, 2))
-    channel_order=[1, 0, 2, 3],
-)  # reordering of epochs.ch_names according to [[0,2],[1,3]] of subplot axes
+fig, ax = plot_conditions(epochs, conditions=conditions, 
+                          ci=97.5, n_boot=1000, title='',
+                          diff_waveform=None, #(1, 2))
+                          channel_order=[1,0,2,3]) # reordering of epochs.ch_names according to [[0,2],[1,3]] of subplot axes
 
 # Manually adjust the ylims
-for i in [0, 2]:
-    ax[i].set_ylim([-0.5, 0.5])
-for i in [1, 3]:
-    ax[i].set_ylim([-1.5, 2.5])
+for i in [0,2]: ax[i].set_ylim([-0.5,0.5])
+for i in [1,3]: ax[i].set_ylim([-1.5,2.5])
+
+
