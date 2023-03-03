adf5e289cd0c212a792a4e259667369fea39c586
Erik Bjäreholt <erik@bjareho.lt>
2023-03-03 15:25:11 +0100

    chore: applied no_implicit_optional

--- eegnb/experiments/visual_vep/vep.py
+++ eegnb/experiments/visual_vep/vep.py
@@ -1,11 +1,12 @@
 from time import time, strftime, gmtime
 from pylsl import StreamInfo, StreamOutlet
 from eegnb.experiments.Experiment import Experiment
+from typing import Optional
 
 
 class VisualVEP(Experiment):
 
-    def __init__(self, duration=120, eeg: EEG=None, save_fn=None,
+    def __init__(self, duration=120, eeg: Optional[EEG]=None, save_fn=None,
             n_trials = 2000, iti = 0.2, soa = 0.2, jitter = 0.1):
         
         exp_name = "Visual VEP"
