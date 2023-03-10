53154c2f32fb286457d537f438bca4f62620e639
Parv Agarwal <65726543+Parvfect@users.noreply.github.com>
2023-03-10 12:41:00 +0000

    Updated doc examples for N170, P300 and SSVEP after Experiment Class Refactor (#218)
    * major update: merging develop to master (#217)
    
    * example test commit (#182)
    
    * example test commit
    
    * example edit
    
    * ci: run test workflow on develop branch
    
    * ci: add develop branch to job triggers
    
    * ci: fix syntax issue in workflow
    
    * fix: fixed import (brainflow updated API)
    
    * build(deps): locked pylsl==1.10.5 (#187)
    
    * Experiment Class Refactor (update to #183), converting specific experiments to subclasses (#184)
    
    * First commit
    
    * Second commit
    
    * Modifications
    
    * Lol
    
    * Lol
    
    * Incorporated N170 and p300, looking good for a PR
    
    * ssvep update
    
    * Implementing subclasses instead of loose functions
    
    * fix: fixed import (brainflow updated API)
    
    * Playing around still
    
    * Fixing import errors
    
    * Adding abstractmethod decorators
    
    * Still working on the import error
    
    * Guess what's finally working
    
    * Comments and naming ticks
    
    * More comments
    
    * Live coding demonstration
    
    * ssvep adapted
    
    * Adapting Auditory Oddball
    
    * changing save_fn to self.save_fun
    
    * This maybe the last big change
    
    * utils file changed, changes work through cli
    
    Co-authored-by: Erik Bjäreholt <erik@bjareho.lt>
    
    * Submodule added for gsoc
    
    * Adding pipelines for cli analysis (#202)
    
    * started pipelines function
    
    * almost working simple function equivalents of nb scripts
    
    * fix: fixed import (brainflow updated API)
    
    * sqc fixes for unicorn (#176)
    
    * Ignore pushes
    
    * Trying to create a cli
    
    * Stepping through the problem
    
    * First commit
    
    * Fixing pause in signal quality check
    
    * Fixing Signal quality check problem
    
    * fix the technical debt
    
    * Save path done for automated saving pdf
    
    * I feel amazing
    
    * Almost through
    
    * Update eegnb/cli/__main__.py
    
    Co-authored-by: Erik Bjäreholt <erik@bjareho.lt>
    
    * Trying to create cli but it's being really painful
    
    * Extra word cli error
    
    * Changed example handling
    
    * Pain
    
    * Adding whole datapath
    
    * Finally fixed cli
    
    * hmm
    
    * Looking good
    
    * added hyperlink
    
    * Having some issues with detecting css and image deltetion
    
    * Just the css now
    
    * Fixed the css linking problem though it's a weird soln
    
    * Automated running, still fnames problem
    
    * Hahahah embedded images in html
    
    * Improving code
    
    * Okay now
    
    * Look at that
    
    * Almost there just the two figures now
    
    * Now
    
    * Added attrdict to do with cli error
    
    Co-authored-by: John Griffiths <j.davidgriffiths@gmail.com>
    Co-authored-by: Erik Bjäreholt <erik@bjareho.lt>
    Co-authored-by: John Griffiths <JohnGriffiths@users.noreply.github.com>
    
    * added more options for site args; improved function names; removed some redundant lines (#209)
    
    * fix subject num parsing bug
    
    * analysis report function improvements for openbci cyton and gtec unicorn devices
    
    * run exp fix
    
    * Update requirements.txt
    
    * fixes to get docs building by github action (#210)
    
    * fixes to get docs building by github action
    
    * reverted some changes
    
    * Update 01r__ssvep_viz.py
    
    Co-authored-by: John Griffiths <JohnGriffiths@users.noreply.github.com>
    
    * Update README.rst
    
    small commit to test doc build workflow on this branch
    
    * removing gsoc submodule
    
    Co-authored-by: Erik Bjäreholt <erik@bjareho.lt>
    Co-authored-by: Parv Agarwal <65726543+Parvfect@users.noreply.github.com>
    Co-authored-by: Parvfect <parvagrw02@gmail.com>
    Co-authored-by: Ben Pettit <pelleter@gmail.com>
    
    * Updated doc examples
    
    * Update 00x__n170_run_experiment.py
    
    fix: typo in func param
    
    ---------
    
    Co-authored-by: John Griffiths <JohnGriffiths@users.noreply.github.com>
    Co-authored-by: Erik Bjäreholt <erik@bjareho.lt>
    Co-authored-by: Ben Pettit <pelleter@gmail.com>
    Co-authored-by: Ore O <oreogundipe@gmail.com>

--- examples/visual_n170/00x__n170_run_experiment.py
+++ examples/visual_n170/00x__n170_run_experiment.py
@@ -12,25 +12,25 @@ an experiment.
 # ---------------------  
 #  
 # Imports
+from eegnb import generate_save_fn
 from eegnb.devices.eeg import EEG
 from eegnb.experiments import VisualN170
 
 # Define some variables
+board_name = "muse2" # board name
+experiment_name = "visual_n170" # experiment name
+subject_id = 0 # test subject id
+session_nb = 0 # session number
+record_duration = 120 # recording duration
+
+# generate save path
+save_fn = generate_save_fn(board_name, experiment_name, subject_id, subject_nb)
+
+# create device object
+eeg_device = EEG(device=board_name)
 
 # Experiment type
-experiment = VisualN170()
-
-# EEG device
-experiment.eeg = EEG(device="cyton")  # "muse")
-
-# Test subject id
-subject_id = 0
-
-# Session number
-session_nb = 0
-
-# Experiment recording duration
-experiment.duration = 120
+experiment = VisualN170(duration=record_duration, eeg=eeg_device, save_fn=save_fn)
 
 ###################################################################################################  
 # Run experiment
@@ -39,4 +39,4 @@ experiment.duration = 120
 experiment.run()
 
 # Saved csv location
-print(experiment.save_fn)
+print("Recording saved in", experiment.save_fn)
