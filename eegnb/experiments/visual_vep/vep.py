bb88718941bcc1516af75fa44e7cfb6d4edbc74d
Ore O <oreogundipe@gmail.com>
2023-04-26 23:56:15 -0700

    fix: update BaseExperiment class reference

--- eegnb/experiments/visual_vep/vep.py
+++ eegnb/experiments/visual_vep/vep.py
@@ -5,7 +5,7 @@ from eegnb.experiments import Experiment
 from eegnb.devices.eeg import EEG
 
 
-class VisualVEP(Experiment):
+class VisualVEP(Experiment.BaseExperiment):
 
     def __init__(self, duration=120, eeg: EEG=None, save_fn=None,
             n_trials = 2000, iti = 0.2, soa = 0.2, jitter = 0.1):
