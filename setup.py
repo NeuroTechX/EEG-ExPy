5bbca2bd8a82bd0792ab066d55f01f0c973fe3bb
John Griffiths <JohnGriffiths@users.noreply.github.com>
2024-06-15 22:48:17 -0400

    Add multiple installation options (#263)
    * added newly separated analysis and streaming utils files
    
    * modified setup and requirements to allow for optional install types
    
    * updated imports in experiment examples
    
    * fixed some typos
    
    * Update requirements.txt
    
    moved click library to stim pres requirements section
    
    * Update requirements.txt
    
    Co-authored-by: Taha Morshedzadeh <t.morshedzadeh@mail.utoronto.ca>
    
    * Update Makefile
    
    * Update docs.yml
    
    * Update Makefile
    
    this does a full build with all dependencies for EEG-ExPy
    
    ---------
    
    Co-authored-by: Ore O <oreogundipe@gmail.com>
    Co-authored-by: Taha Morshedzadeh <t.morshedzadeh@mail.utoronto.ca>

--- setup.py
+++ setup.py
@@ -5,8 +5,11 @@ from setuptools import setup, find_packages
 with open("README.rst", "r") as fh:
     long_description = fh.read()
 
-with open('requirements.txt', 'r') as fp:
-    install_requires = fp.read().splitlines()
+fptxt = open('requirements.txt', 'r').read()
+install_requires_analysis = fptxt.split('## ~~ Analysis Requirements ~~')[1].split('## ~~')[0].splitlines()[1:]
+install_requires_streaming = fptxt.split('## ~~ Streaming Requirements ~~')[1].split('## ~~')[0].splitlines()[1:]
+install_requires_stimpres = fptxt.split('## ~~ Stimpres Requirements ~~')[1].split('## ~~')[0].splitlines()[1:]
+install_requires_docsbuild = fptxt.split('## ~~ Docsbuild Requirements ~~')[1].split('## ~~')[0].splitlines()[1:]
 
 setup(
     name="eeg-expy", 
@@ -17,7 +20,14 @@ setup(
     keywords='eeg, cognitive neuroscience, experiments, evoked response, auditory, visual',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    install_requires = install_requires,
+    install_requires=[ install_requires_analysis ],   # base dependencies
+    extras_require={
+        'docsbuild':  install_requires_docsbuild,
+        'streaming':  install_requires_streaming,
+        'stimpres':   install_requires_stimpres,
+        'streamstim': install_requires_streaming + install_requires_stimpres,
+        'full':       install_requires_docsbuild + install_requires_streaming + install_requires_stimpres 
+                   },
     url='https://github.com/NeuroTechX/eeg-expy',
     license="BSD (3-clause)",
     entry_points={"console_scripts": ["eegnb=eegnb.cli.__main__:main",
