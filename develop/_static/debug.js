f443801bf8c7e26fb25d4514ab13e197224c39fd
oreHGA <oreHGA@users.noreply.github.com>
2024-09-05 19:39:23 +0000

    deploy: ab1a444a7a104793533defa38aff1440784a4054

--- develop/_static/debug.js
+++
@@ -1,7 +0,0 @@
-// Add debug actions to flyout menu
-
-$(function () {
-  $("[data-toggle='rst-debug-badge']").on("click", function () {
-    $("[data-toggle='rst-versions']").toggleClass("rst-badge");
-  });
-})
