dab4551a7a691a5fec0a7907a6c27f1939c2970b
Ben Pettit <benpettit@digimulti.com>
2025-07-16 21:48:56 +1000

    Added a customizable `site` parameter into `generate_save_fn` so it can be used and match with the `get_recording_dir` signature.

--- eegnb/__init__.py
+++ eegnb/__init__.py
@@ -47,11 +47,12 @@ def generate_save_fn(
     experiment: str,
     subject_id: int,
     session_nb: int,
+    site="local",
     data_dir=DATA_DIR,
 ) -> Path:
     """Generates a file name with the proper trial number for the current subject/experiment combo"""
     recording_dir = get_recording_dir(
-        board_name, experiment, subject_id, session_nb, data_dir=data_dir
+        board_name, experiment, subject_id, session_nb, site, data_dir=data_dir
     )
 
     # generate filename based on recording date-and-timestamp and then append to recording_dir
